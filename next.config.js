4d24a432a6c870e909653b04506240916b7391df
J <jtliu@JT-MacBook-Pro.local>
2025-08-01 00:16:21 -0700

    update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,34 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/favicon.(ico|png|svg)',
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/favicon.svg',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/favicon-16x16.png',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=86400, must-revalidate',
+          },
+        ],
+      },
+      {
+        source: '/favicon-32x32.png',
         headers: [
           {
             key: 'Cache-Control',
